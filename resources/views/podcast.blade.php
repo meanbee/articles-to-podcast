4bd9124992492fa0775915569a5efd00e971e2ec
Nick Jones <nick@nicksays.co.uk>
2015-05-20 19:57:50 +0100

    Add short tag support

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0"?>
+<?php echo '<?xml version="1.0"?>'; ?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
     <channel>
         <title>{{ $user->name }}'s Articles</title>

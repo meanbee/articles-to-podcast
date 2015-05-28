1969f2a169214c7a730c7e69a2b4520f28eb37d0
Tom Robertshaw <me@tomrobertshaw.net>
2015-05-28 08:40:13 +0100

    Update podcase feed to:
    - Work off username rather than email which we don't have anymore
    - As we don't have email, use gravatar image of pocket support instead
    - Add excerpt
    - Correct S3 urls.

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -1,24 +1,24 @@
 <?php echo '<?xml version="1.0"?>'; ?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
     <channel>
-        <title>{{ $user->name }}'s Articles</title>
+        <title>{{ $user->username }}'s Articles</title>
         <link>{{ route('podcast', array('id' => $user->id, 'secret' => $user->secret())) }}</link>
-        <description>{{ $user->name }}'s articles, converted into speech.</description>
+        <description>{{ $user->username }}'s articles, converted into speech.</description>
         <language>en-gb</language>
         <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
         <lastBuildDate>{{ date(DATE_RFC2822) }}</lastBuildDate>
         <generator>https://github.com/meanbee/articles-to-podcast</generator>
-        <managingEditor>{{ $user->email }}</managingEditor>
-        <webMaster>{{ $user->email }}</webMaster>
-        <itunes:image>http://www.gravatar.com/avatar/{{ md5(strtolower($user->email)) }}?s=500</itunes:image>
+        <managingEditor>{{ $user->username }}</managingEditor>
+        <webMaster>{{ $user->username }}</webMaster>
+        <itunes:image>http://www.gravatar.com/avatar/{{ md5(strtolower('support@getpocket.com')) }}?s=500</itunes:image>
         @foreach ($items as $item)
             <item>
-                <enclosure url="https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ $item->id }}.mp3" type="audio/mpeg" />
+                <enclosure url="https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3" type="audio/mpeg" />
                 <title>Item '{{ $item->title }}'</title>
-                <link>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ $item->id }}.mp3</link>
+                <link>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</link>
                 <description>{{ $item->excerpt }}</description>
                 <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
-                <guid>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ $item->id }}.mp3</guid>
+                <guid>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</guid>
             </item>
         @endforeach
     </channel>

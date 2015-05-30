3d033ffd6d65f30ad0c6680468f042891851108c
Tom Robertshaw <me@tomrobertshaw.net>
2015-05-30 21:28:11 +0100

    Change links to button in dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,15 +5,17 @@
     <h2>Dashboard</h2>
 
 	<p>
-        <a href="{{ str_replace('http://', 'itpc://', route('podcast', array('id' => $user->id, 'secret' => $user->secret()))) }}">Subscribe to your podcast</a>.
+        <button class="button" onclick="window.location.href = \"{{ str_replace('http://', 'itpc://', route('podcast', array('id' => $user->id, 'secret' => $user->secret()))) }}\">Subscribe</button>
 	</p>
 
-    <p>
-        <a href="{{ route('pocket.synchronise') }}">Synchronise with Pocket</a>
-    </p>
+    <p>Articles are automatically fetched, convert to podcast episodes and added to your feed every hour.</p>
+
 
     @if (count($items) > 0)
         @include('partials.items', array('items' => $items))
+        <p><button class="button" onclick="window.location.href = \"{{ route('pocket.synchronise') }}\"">Update</button></p>
+    @else
+        <p><button class="button" onclick="window.location.href = \"{{ route('pocket.synchronise') }}\"">Get pocket articles</button></p>
     @endif
 </section>
 @endsection

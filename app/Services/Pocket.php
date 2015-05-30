f8941940bd06c07092b7c72fe9c6183be49b9ebf
Tom Robertshaw <me@tomrobertshaw.net>
2015-05-30 21:34:57 +0100

    Remove duplicate function.

--- app/Services/Pocket.php
+++ app/Services/Pocket.php
@@ -161,70 +161,7 @@ class Pocket {
 
         return $item;
     }
-
-    /**
-     * Get article body text
-     * First try Goose, then try Readability
-     *
-     * @param $url
-     * @return string
-     */
-    protected function getArticleContent($url)
-    {
-        $goose = new GooseClient();
-
-        try {
-            $article = $goose->extractContent($url);
-            $articleText = $article->getCleanedArticleText();
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-            return false;
-        }
-
-        if (!$articleText) {
-            // If Goose failed, try Readability
-            $html = $article->getRawHtml();
-
-            if (function_exists('tidy_parse_string')) {
-                $tidy = tidy_parse_string($html, array(), 'UTF8');
-                $tidy->cleanRepair();
-                $html = $tidy->value;
-            }
-
-            $readability = new Readability($html, $url);
-            $readability->debug = false;
-            $result = $readability->init();
-
-            if ($result) {
-                $articleText = $readability->getContent()->innerHTML;
-                // if we've got Tidy, let's clean it up for output
-                if (function_exists('tidy_parse_string')) {
-                    $tidy = tidy_parse_string($articleText, array('indent' => true, 'show-body-only' => true), 'UTF8');
-                    $tidy->cleanRepair();
-                    $articleText = $tidy->value;
-                }
-            }
-        }
-
-        $articleText = trim(strip_tags($articleText));
-
-        return $articleText;
-    }
-
-    /**
-     * Replace utf8mb4 with utf8
-     * @param $string
-     *
-     * @return mixed
-     */
-    protected function replace4byte($string) {
-        return preg_replace('%(?:
-          \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
-        | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-        | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
-        )%xs', '', $string);
-    }
-
+    
 
     /**
      * Get the http client.

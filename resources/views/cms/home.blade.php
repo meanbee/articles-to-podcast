67b9911e077b24d2997407d7a2261e72c64673db
Tom Robertshaw <me@tomrobertshaw.net>
2015-05-30 21:35:59 +0100

    Add info on homepage over what the service is and style it centrally.

--- resources/views/cms/home.blade.php
+++ resources/views/cms/home.blade.php
@@ -6,6 +6,8 @@
 @section('content')
 <section class="main-inner">
     <div class="pocket-login-promo">
+        <h2>Blog reading stacking up?</h2>
+        <p>Subscribe to a podcast feed of your pocket articles for listening on the go!</p>
         @if(!$auth->check() )
             <a href="{{ url('/pocket/login') }}" class="pocket-login">Login with<span class="pocket-logo">Pocket</span></a>
         @endif

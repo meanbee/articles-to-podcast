8118a493e213b76aa8129dd5979d62c0cbd9b48f
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-03 23:34:19 +0100

    Update podcast feed spec based on feedback from validator and comparing to other itunes feeds.

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -2,21 +2,29 @@
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
     <channel>
         <title>{{ $user->username }}'s Articles</title>
-        <link>{{ route('podcast', array('id' => $user->id, 'secret' => $user->secret())) }}</link>
+        <link>http://articles2podcast.herokuapp.com</link>
         <description>{{ $user->username }}'s articles, converted into speech.</description>
         <language>en-gb</language>
         <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
         <lastBuildDate>{{ date(DATE_RFC2822) }}</lastBuildDate>
         <generator>https://github.com/meanbee/articles-to-podcast</generator>
-        <managingEditor>{{ $user->username }}</managingEditor>
-        <webMaster>{{ $user->username }}</webMaster>
-        <itunes:image>http://www.gravatar.com/avatar/{{ md5(strtolower('support@getpocket.com')) }}?s=500</itunes:image>
-        @foreach ($items as $item)
+        <copyright>Copyright </copyright>
+        <itunes:image href="{{ asset('assets/images/pocket-logo-large.jpg') }}" />
+        <itunes:subtitle>Pocket Audio</itunes:subtitle>
+        <itunes:author>ArticleCast</itunes:author>
+        <itunes:explicit>no</itunes:explicit>
+        <image>
+            <url>{{ asset('assets/images/pocket-logo-large.jpg') }}</url>
+            <title>{{ $user->username }}'s Articles</title>
+            <link>{{ route('podcast', array('id' => $user->id, 'secret' => $user->secret())) }}</link>
+        </image>
+
+    @foreach ($items as $item)
             <item>
                 <enclosure url="https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3" type="audio/mpeg" />
-                <title>{{ $item->title }}</title>
+                <title><![CDATA[{{ $item->title }}]]></title>
                 <link>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</link>
-                <description>{{ $item->excerpt }}</description>
+                <description><![CDATA[{{ $item->excerpt }}]]></description>
                 <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
                 <guid>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</guid>
             </item>

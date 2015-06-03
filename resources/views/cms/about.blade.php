3990ad03281e28d4bae3c172f4ffba79355d1cef
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-03 17:26:34 +0100

    Update title of the about page.

--- resources/views/cms/about.blade.php
+++ resources/views/cms/about.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
-@section('title', 'Home')
-@section('description', 'Converter to create mp3 podcasts to subcribe in itunes for your articles.')
+@section('title', 'About')
+@section('description', 'Find out more about why we created Articles to Podcast with Pocket.')
 
 @section('content')
     <section class="main-inner">
-        <p>Created on a <a href="http://www.meetup.com/Meanbee-Hack-Nights/">Meanbee Hacknight</a>.</p>
+        <p>Created on a <a href="http://www.meetup.com/Meanbee-Hack-Nights/">Meanbee hack night</a>.</p>
 
         <p>Problem: Lots of saved articles to read but I have more time to listen to audio, e.g. commuting, than I do to sit down and read. When I have time to read I'm normally distracted with other things.</p>
 

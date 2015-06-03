d19596951d4c7590843b30678b68842e55b3c543
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-04 00:03:11 +0100

    Add length to feed item and only include in feed if mp3s exist.

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -20,14 +20,16 @@
         </image>
 
     @foreach ($items as $item)
-            <item>
-                <enclosure url="https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3" type="audio/mpeg" length="1" />
-                <title><![CDATA[{{ html_entity_decode($item->title) }}]]></title>
-                <link>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</link>
-                <description><![CDATA[{{ html_entity_decode($item->excerpt) }}]]></description>
-                <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
-                <guid>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</guid>
-            </item>
+            @if (file_exists(storage_path() . "/app/" . md5($item->url) . ".mp3"))
+                <item>
+                    <enclosure url="https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3" type="audio/mpeg" length="{{ filesize(storage_path() . "/app/" . md5($item->url) . ".mp3") }}" />
+                    <title><![CDATA[{{ $item->title }}]]></title>
+                    <link>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</link>
+                    <description><![CDATA[{{ $item->excerpt }}]]></description>
+                    <pubDate>{{ date(DATE_RFC2822) }}</pubDate>
+                    <guid>https://s3-eu-west-1.amazonaws.com/articles-to-podcast/{{ md5($item->url) }}.mp3</guid>
+                </item>
+            @endif
         @endforeach
     </channel>
 </rss>

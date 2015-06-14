145a6965bfb31f0ea4f575f39db22417bf20049d
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-14 20:01:15 +0100

    Add regular RSS link to subscribe to on dashboard.

--- resources/views/cms/about.blade.php
+++ resources/views/cms/about.blade.php
@@ -9,7 +9,7 @@
 
         <p>Problem: Lots of saved articles to read but I have more time to listen to audio, e.g. commuting, than I do to sit down and read. When I have time to read I'm normally distracted with other things.</p>
 
-        <p>Solution: Use saved URLs in pocket account, visit URL and scrape body content, convert to audio, save as mp3, host and create podcast feed to be able to subscribe to in iTunes (or similar)</p>
+        <p>Solution: Use saved URLs in pocket account, visit URL and scrape body content, convert to audio, save as mp3, host and create podcast feed to be able to subscribe to in iTunes (or similar).</p>
     </section>
 @stop
 @section('before_body_end')
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,10 @@
 <section class="main-inner">
     <h2>Dashboard</h2>
 
-	<p>
-        <a class="button" href="{{ str_replace('http://', 'itpc://', route('podcast', array('id' => $user->id, 'secret' => $user->secret()))) }}">Subscribe</a>
-	</p>
+	<div class="subscribe">
+        <a class="button subscribe-button" href="{{ str_replace('http://', 'itpc://', route('podcast', array('id' => $user->id, 'secret' => $user->secret()))) }}">Subscribe with iTunes</a>
+        <a class="subscribe-link" href="{{ route('podcast', array('id' => $user->id, 'secret' => $user->secret())) }}">Subscribe with RSS</a>
+    </div>
 
     <p>Articles are automatically fetched, convert to podcast episodes and added to your feed every hour.</p>
     <p>Once you've listened to them, you'll need to manually mark them as read in <a href="http://getpocket.com">Pocket</a>.</p>

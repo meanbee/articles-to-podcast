1d506fce91801411fad2f2b8646e11af6a903136
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-15 10:28:38 +0100

    Add link to write-up.

--- resources/views/cms/about.blade.php
+++ resources/views/cms/about.blade.php
@@ -10,6 +10,7 @@
         <p>Problem: Lots of saved articles to read but I have more time to listen to audio, e.g. commuting, than I do to sit down and read. When I have time to read I'm normally distracted with other things.</p>
 
         <p>Solution: Use saved URLs in pocket account, visit URL and scrape body content, convert to audio, save as mp3, host and create podcast feed to be able to subscribe to in iTunes (or similar).</p>
+        <p>Write up on <a href="http://tomrobertshaw.net/2015/06/articlecast-with-pocket/">tomrobertshaw.net</a></p>
     </section>
 @stop
 @section('before_body_end')
